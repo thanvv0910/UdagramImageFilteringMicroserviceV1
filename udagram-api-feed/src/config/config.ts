de77e0386a3eb68876a742bdd5b12573a1733ec1
Vu Viet Than <thanlo0910@gmail.com>
2024-09-03 00:49:41 +0700

    inital commit

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,15 @@
+export const config = {
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  aws_region: process.env.AWS_REGION,
+  aws_access_key: process.env.AWS_ACCESS_KEY,
+  secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
